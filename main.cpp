9fb6318089d14bb0c9946cf763c3da524c068cf3
Jean Tampon <jean.tampon@gmail.com>
2019-03-24 09:46:52 +0100

    Add test main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "event_manager.hpp"
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Test window");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	EventManager evm(window);
+	evm.registerCallback(sf::Event::EventType::Closed, [&]() {window.close(); });
+
+	while (window.isOpen())
+	{
+		evm.processEvents();
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
